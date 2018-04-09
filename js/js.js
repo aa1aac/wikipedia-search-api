6d51959c238022d94a244ce102c77d716be43ad6
aa11ac <ach.aayush.111@gmail.com>
2018-04-09 14:04:19 +0545

    change functionality

--- js/js.js
+++ js/js.js
@@ -2,30 +2,33 @@ $(document).ready(function(){
     var searchList=document.querySelector("searchList");
 var searchItem=document.querySelector("input");
 searchItem.addEventListener("keypress",function(e){
-    
         var key = e.which || e.keyCode;
         if (key === 13) {
-            var input = $('.input').val();
+       
+            var input = $('input').val();
   var apiUrl="https://en.wikipedia.org/w/api.php?action=opensearch&search="+input+"&format=json&callback=?";
     console.log (apiUrl);
-     $.get(apiUrl)
-      .done(function(data){
-          for(var i=0;i<10;data[1][i].length){
-          var result=data[1][i];
-          var content=data[2][i];
-          var href=data[3][i];
-          searchList.html('<li class="container"><a href="'+href+'"><h1>'+result+'</h1></a><p class="container">'+content+'</p></li>')
-
-          }
-        })
-      .fail(function(){
-          alert("Opps some error occured")
+    $.ajax({
+        type:"GET",
+        url:apiUrl,
+        async:false,
+        dataType:"json",
+        success: function(data){
+          $('ul').html(" ");
+          
+          for(var i=0; i < data[1].length;i++){
+          $('ul').prepend("<li><a href="+data[3][i]+" target=" + "blank" + "><h4>" +data[1][i]+"</h1></a><p>"+data[2][i]+"</p></li>");
+        }
+          
+          $('#search_botton').val('');
+        },
+        error:function(errorMessage){
+          alert("Error");
+      }
+     
     
 
         })
     }
       });
             });
-    
-  
-    
